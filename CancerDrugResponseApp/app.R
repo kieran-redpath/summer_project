3fa02c1e6beb78a0c6b342754ce91fcec6c621dd
Kieran Redpath <57608799+kieran-redpath@users.noreply.github.com>
2020-01-28 15:37:21 +1300

    updates

--- CancerDrugResponseApp/app.R
+++ CancerDrugResponseApp/app.R
@@ -22,17 +22,13 @@ library(devtools)
 # Load Data (outside the Shiny App)
 # Load CCLE
 ccleExpData <- read.gct('data/CCLE_RNAseq_genes_counts_20180929.gct')
+logExpDat <- log(ccleExpData + 0.5)
+dge <- DGEList(counts=ccleExpData)
+dge <- calcNormFactors(dge)
+dge_voom <- voom(dge, plot=FALSE)
+expDat <- dge_voom$E
 # Load GDSC
 GDSC2 <- fread('data/GDSC2_fitted_dose_response_15Oct19.csv', sep= ';')
-
-
-
-
-
-
-
-
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -48,17 +44,20 @@ ui <- fluidPage(
                ),
                
                textInput("cell_types",
-                         label = h3("Enter cell line tissue type, separated by commas. A full list of supported names can be found",
-                                  a("here.", href = "INSERT LINK HERE")),
-                         value = "Enter CCLE names..."
+                         label = h3("Enter cell line tissue type, separated by commas. A full list of supported tissues can be found",
+                                    a("here.", href = "INSERT LINK HERE")),
+                         value = "Enter tissue types..."
                )
       )
     ),
     
     # Main panel containing the generated heatmap
     mainPanel(
-      # plotOutput(outputID = "heatmap"),
-      p("words.")
+      tabsetPanel(
+        tabPanel("Enriched Pathways", tableOutput("pathways")),
+        tabPanel("Overlap Between Pathways", plotOutput("overlap")),
+        tabPanel("Heatmaps of Top 5 Pathways", plotOutput("heatmap"))
+      )
     )
   )
 )
@@ -74,10 +73,63 @@ ui <- fluidPage(
 
 # Server that actually processes the data, with render calls etc.
 server <- function(input, output) {
+  
+  # Should use: input$drug_name (it does now), input$cell_types
+  # Most of the code you have should go here.
+  # It can be tidied up later so only the important parts are reactive, not everything.
+  # This expression must return an object (the plot)
+  
+  # TAKE INPUT$DRUG_NAME and create the drug data file
+  drug <- GDSC2 %>% filter(grepl(input$drug_name, DRUG_NAME, ignore.case=TRUE))
+  expDatsplitnames <- strsplit(colnames(expDat), "_") %>% 
+    lapply(., function(x) x[1]) %>% 
+    unlist()
+  expDatsplit <- expDat
+  colnames(expDat) <- expDatsplitnames
+  drug$CELL_LINE_NAME <- gsub("-","",drug$CELL_LINE_NAME, fixed=TRUE)
+  commonSamples <- intersect(drug$CELL_LINE_NAME,colnames(expDat))
+  expDat_match <- match(commonSamples, colnames(expDat))
+  expDat_sort <- expDat[ , na.omit(expDat_match)]
+  drug_match <- match(commonSamples, drug$CELL_LINE_NAME)
+  drug_sort <- drug[na.omit(drug_match),]
+  colnames(drug_sort)[5] <- "CCLE_Name"
+  
+  # MAKE TISSUETOOL, AND ADD A COLUMN TO IT SO IT CAN CONVERT TO CELL TYPE IDENTIFIER
+  lng <- length(colnames(expDatsplit))
+  tissuetool <-data.frame(Cell_Line=character(lng),Tissue_Type=character(lng))
+  tissuetool$Cell_Line <- strsplit(colnames(expDatsplit), "_") %>% 
+    lapply(., function(x) x[1]) %>% 
+    unlist()
+  tissuetool$Tissue_Type <- strsplit(colnames(expDatsplit), "_") %>% 
+    lapply(., function(x) x[-1])%>%
+    paste(., sep="_", collapse = NULL) %>% 
+    unlist()
+  tissuetool$Tissue_Type <- gsub('c', '', tissuetool$Tissue_Type)
+  tissuetool$Tissue_Type <- gsub('[[:punct:]]', '', tissuetool$Tissue_Type)
+  tissuetool$Tissue_Type <- gsub(' ', '_', tissuetool$Tissue_Type)
+  drug_help <- drug_sort %>% subset(., select=c("CCLE_Name", "TCGA_DESC"))
+  drug_help <- drug_help %>% dplyr::rename(., Cell_Line = CCLE_Name)
+  tissuetool <- inner_join(tissuetool, drug_help, by="Cell_Line")
+  
+  #LOOPS ARE MISSING! ON THE OTHER DOCUMENT THOUGH
+  
+  
+  
+  
+  
+  
+  
+  
+  output$pathways <- renderTable({
+    # goseqPathways code goes here
+  })
+  
+  output$overlap <- renderPlot({
+    # overlap of pathways heatmap goes here
+  })
+  
   output$heatmap <- renderPlot({
-    # Should use: input$drug_name, input$cell_types_CCLE, input$cell_types_GDSC
-    # Most of the code you have should go here.
-    # It can be tidied up later so only the important parts are reactive, not everything.
+    # heatmap generating code goes here. Only want the top 5-10
   })  
   
 }
